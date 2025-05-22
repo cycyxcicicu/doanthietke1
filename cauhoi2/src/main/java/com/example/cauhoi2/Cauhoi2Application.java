e12e620094dec5589177875aff7f5e66b3932660
Thuong <96791491+nguyenvanthuong10112003@users.noreply.github.com>
2025-05-23 00:40:06 +0700

    merge from branch duc2003 into main

--- cauhoi2/src/main/java/com/example/cauhoi2/Cauhoi2Application.java
+++ cauhoi2/src/main/java/com/example/cauhoi2/Cauhoi2Application.java
@@ -2,10 +2,7 @@ package com.example.cauhoi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
-@EnableFeignClients
 @SpringBootApplication
 public class Cauhoi2Application {
 	public static void main(String[] args) {
--- cauhoi2/src/main/java/com/example/cauhoi2/dto/response/QuestionResponse.java
+++ cauhoi2/src/main/java/com/example/cauhoi2/dto/response/QuestionResponse.java
@@ -1,4 +1,4 @@
-package com.example.cauhoi2.dto.response.file_data;
+package com.example.cauhoi2.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
